dbded0ec97b11db93779ba8dd9211cbc2d5419f7
Eileen Fordham <fordhame17@cs.grinnell.edu>
2014-05-29 16:21:53 -0500

    Finish HSV Conversion and Save Image Button (incomplete)

--- Sandbox/render.js
+++ Sandbox/render.js
@@ -8,6 +8,56 @@
 // +-----------+
 
 /**
+ * Convert a value in the range [-1 .. 1] to a hue.
+ */
+function r2h(r)
+{
+    return 180 + r*180;
+}
+
+/**
+ * Convert a value in the range [-1 .. 1] to a saturation or value.
+ */
+function r2sv(r)
+{
+    return .5 + r*.5;
+}
+
+/**
+ * Converts hsv values to rgb values.
+ */
+function hsv2rgb(h, s, v)
+{
+    var rgb = [];
+    if (s === 0)
+        rgb = [v, v, v];
+    else
+    {
+        var c = v * s;
+        var h2 = h / 60;
+        var x = c*( 1 - Math.abs((h2 % 2) - 1));
+        if (h2 >= 5)
+            rgb = [c, 0, x];
+        else if (h2 >= 4)
+            rgb = [x, 0, c];
+        else if (h2 >= 3)
+            rgb = [0, x, c];
+        else if (h2 >= 2)
+            rgb = [0, c, x];
+        else if (h2 >= 1)
+            rgb = [x, c, 0];
+        else
+            rgb = [c, x, 0];
+    }
+    var m = v - c;
+    rgb[0] = r2c (rgb[0] + m);
+    rgb[1] = r2c (rgb[1] + m);
+    rgb[2] = r2c (rgb[2] + m);
+    return rgb;
+}
+
+
+/**
  * Convert a value in the range [-1 .. 1] to a component.
  */
 function r2c(r)
@@ -264,6 +314,49 @@ function renderRGB(rfun, gfun, bfun, canvas, rleft, rtop, rwidth, rheight)
     } // for
   context.putImageData(region, rleft, rtop);
 } // renderRGB
+
+/**
+ * Render three (x, y)->range functions as HSV, given the specified 
+ * bounds. Intended mostly as an experiment to get started.
+ */
+function renderHSV(hfun, sfun, vfun, canvas, rleft, rtop, rwidth, rheight)
+{
+  var deltaX = 2.0/rwidth;
+  var deltaY = 2.0/rheight;
+  var context = canvas.getContext("2d");
+  var region = context.createImageData(rwidth,rheight);
+  var d = new Date();
+  var sec = d.getMilliseconds()/500 - 1;
+  var min = (d.getSeconds()*1000 + d.getMilliseconds())/30000 - 1;
+  var time = new Time(sec,min);
+  var mouse = new Mouse((mouseX-rleft)*deltaX - 1, 
+                        (mouseY-rtop)*deltaY - 1, 
+                        (clickX-rleft)*deltaX - 1, 
+                        (clickY-rtop)*deltaY - 1);
+  var x = -1;
+  var y = -1 - deltaY;
+    for (var i = 0; i < region.data.length; i+= 4)
+    { // When we reach the end of the row, move on to the next row
+        if ((i % (4*rwidth)) == 0)
+        { 
+            x = -1;
+            y += deltaY;
+        } // if (i % (4*rwidth)) == 0
+        h = r2h(cap(hfun(x,y,time,mouse)));
+        s = r2sv(cap(sfun(x,y,time,mouse)));
+        v = r2sv(cap(vfun(x,y,time,mouse)));
+
+        var rgb = [];
+        rgb = hsv2rgb(h, s, v);
+        region.data[i+0] = rgb[0];
+        region.data[i+1] = rgb[1];
+        region.data[i+2] = rgb[2];
+        region.data[i+3] = 255;
+        
+        x += deltaX;
+    } // for
+  context.putImageData(region, rleft, rtop);
+} // renderHSV
 
 /**
  * Render an (x,y)->range function on the canvas, given the specified 
