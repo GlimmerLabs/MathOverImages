49678dbce4bd97f537d3c0c9c91b304d986a6cfd
Alexthemitchell <mitchell17@grinnell.edu>
2014-06-05 10:51:03 -0500

    Finalize node.js framework, prepare routes

---
+++ Site/mist/config/auth.js
@@ -0,0 +1,7 @@
+module.exports{
+    "mysql-username":"mist",
+    "mysql-password":"Aukbp?C27Kh{_D+j$#p04!sPk#IL6Y",
+    "mysql-hostname":"localhost"
+
+
+}
---
+++ Site/mist/server.js
@@ -0,0 +1,51 @@
+// server.js
+
+//==================== setup ====================
+
+var express = require('express');
+var morgan = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var expressSession = require('express-session');
+
+
+
+var https = require('https');
+var http = require('http');
+var app = express();
+var port = process.env.PORT || 8080;
+var mysql = require('mysql');
+var passport = require('passport');
+var flash = require('connect-flash');
+
+var configDB = require('./config/database.js');
+
+//==================== configuration ====================
+
+//mysql.connect(configDB.url); // connect to the database
+
+// require(./config/passport')(passport); // pass passport for configuration
+
+
+
+    app.use(morgan('dev')); //log every request to the console
+    app.use(cookieParser()); // read cookie information (for auth)
+    app.use(bodyParser()); // get info from html forms
+
+    app.use(expressSession({secret: 'secret'})); // session secret
+    app.use(passport.initialize());
+    app.use(passport.session());
+    app.use(flash());
+
+    
+
+//==================== routes ====================
+
+require('./app/routes.js')(app,passport);
+
+//==================== launch ====================
+
+app.listen(port);
+console.log('Operating on port '+ port);
+
+
