35c23f3d725d5c6b2137128f7bcc25dad6c3c03f
alexthemitchell <mitchell17@grinnell.edu>
2014-06-05 22:54:48 -0500

    Prepare node.js for sessions/cookies

--- Site/mist/app/routes.js
+++ Site/mist/app/routes.js
@@ -1,41 +1,42 @@
 // app/routes.js
 module.exports = function(app,passport,database) {
+  // HOME PAGE
 
-    // HOME PAGE
+  app.get('/', function(req, res) {
+    //database.addUser("Halley","Freger", "Dawg", "hf@google.com", null, "halley", "1994",function(response){
+    //console.log(response)
+    //});
+    res.end("done");
+  });
 
-    app.get('/', function(req, res) {
-	database.addUser("Halley","Freger", "Dawg", "hf@google.com", null, "halley", "1994",function(response){
-	console.log(response)
-	});
-	res.end("done");
-    });
+  // LOGIN PAGE
 
-    // LOGIN PAGE
+  app.get('/login', function(req,res){
 
-    app.get('/login', function(req,res){
-	
-	res.end("Login");
+    res.cookie('username', 'alexthemitchell').send('<h1>done</h1>');
 
-    });
+    res.end("Login");
 
-    app.get('/logout', function(req,res){
-	req.logout();
-	res.redirect('/');
-    });
-    
-    app.get('/saveImage', function(req,res){
-	res.setHeader('Content-type', 'text/plain');
-	res.setHeader('Content-disposition', 'attachment;filename="filename.txt"');
-	res.end('This is my file.\nIs it not cool?');
-    });
+  });
 
-    
+  app.get('/logout', function(req,res){
+    req.logout();
+    res.redirect('/');
+  });
+
+  app.get('/saveImage', function(req,res){
+    res.setHeader('Content-type', 'text/plain');
+    res.setHeader('Content-disposition', 'attachment;filename="filename.txt"');
+    res.end('This is my file.\nIs it not cool?');
+  });
+
+
 };
 
 function isLoggedIn(req,res,next) {
-    if(req.isAuthenticated())
-	return next();
+  if(req.isAuthenticated())
+    return next();
 
-    res.redirect('/');
+  res.redirect('/');
 
 }
