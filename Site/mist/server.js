56c4ed6a182c284a9aca8e84d0bc20c5eca1d249
Alexthemitchell <mitchell17@grinnell.edu>
2014-06-06 22:50:12 -0500

    Add login and signup functionality (for real this time).

--- Site/mist/server.js
+++ Site/mist/server.js
@@ -13,7 +13,7 @@ var expressSession = require('express-session');
 var https = require('https');
 var http = require('http');
 var app = express();
-var port = process.env.PORT || 8080;
+var port = 8080;
 var mysql = require('mysql');
 var passport = require('passport');
 var flash = require('connect-flash');
@@ -30,18 +30,15 @@ var database = require('./config/database.js');
 
 // require(./config/passport')(passport); // pass passport for configuration
 
+app.use(morgan('dev')); //log every request to the console
+app.use(cookieParser()); // read cookie information (for auth)
+app.use(bodyParser()); // get info from html forms
 
+app.use(expressSession({secret: auth["session-secret"]})); // session secret
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
 
-    app.use(morgan('dev')); //log every request to the console
-    app.use(cookieParser()); // read cookie information (for auth)
-    app.use(bodyParser()); // get info from html forms
-
-    app.use(expressSession({secret: auth["session-secret"]})); // session secret
-    app.use(passport.initialize());
-    app.use(passport.session());
-    app.use(flash());
-
-    
 
 //==================== routes ====================
 
