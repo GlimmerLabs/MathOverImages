ceb69074025b5fca0a5d4f81009f1566a3db13e5
alexthemitchell <mitchell17@grinnell.edu>
2014-06-09 15:25:32 -0500

    Add basic header/footer functions

--- Site/functions/framework.js
+++ Site/functions/framework.js
@@ -1,5 +1,8 @@
 module.exports.anonymousHeader = (function (title, style) {
-var string = "<html><head><title>"+ title + "</title><style>@import url('http://glimmer.grinnell.edu:8080/css/" + style + "');</style></head><body<h1>MIST</h1><h2> by glimmer</h2></body></html>";
+  return "<!DOCTYPE html><html><head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\"><title>" + title + "</title><style>@import url(\"http://glimmer.grinnell.edu:8080/css/"+ style + ");</style></head><body><header><div id = 'headerLogo'><a href='#'><img src='glimmer.grinnell.edu:8080/images/logos/header.svg' /></a></div><nav><ul><li><a href='#'>create</a></li><li><a href='#'>challenges</a></li><li><a href='#'>gallery</a></li><li><a href='#'>about</a></li><li><a href='#'>login/signup</a></li></ul></nav></header>";
+});
 
-return string;
+
+module.exports.stdFooter = (function(){
+return "<footer><p>©2014 Glimmer Labs</p><nav><ul><li><a href='#'>create</a></li><li><a href='#'>challenges</a></li><li><a href='#'>gallery</a></li><li><a href='#'>about</a></li><li><a href='#'>login</a></li><li><a href='#'>signup</a></li><li><a href='#'>license</a></li><li><a href='#'>privacy policy</a></li></ul></nav></footer></body></html>";
 });
