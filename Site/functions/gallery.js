16b0c614a0c4e1b41211d432be56ef75fd554f98
alexthemitchell <mitchell17@grinnell.edu>
2014-06-18 14:47:22 -0500

    Add gallery.jade functionality

--- Site/functions/gallery.js
+++ Site/functions/gallery.js
@@ -1,37 +1,43 @@
 /**
  * Functions related to the gallery.
  */
-module.exports.buildPage = function(req, res, database) {
-  res.render('../public/views/gallery.jade',{
-    loggedIn: req.session.loggedIn,
-    user: req.session.user,
-    generateNarrative: (function() { return "Look.  Pretty pictures." })
+var filedatabase;
+module.exports.buildPage = (function(req, res, database) {
+  filedatabase=database;
+  module.exports.getRandomImages (9, function(images, error){
+
+    res.render('../public/views/gallery.jade',{
+      loggedIn: req.session.loggedIn,
+      user: req.session.user,
+      images: images
+    });
   });
-};
+});
 
 /**
  * Access random images in the gallery.
  */
 /*
-  Procedure: 
+  Procedure:
   gallery.getRandomImages(count, callback));
-  Purpose: 
+  Purpose:
   Grabs random images from the image table and returns the information
-  Parameters: 
+  Parameters:
   count, how many images to return
   callback, a function describing what to do with the results
-  Produces: 
-  rows
-  Pre-conditions: 
+  Produces:
+  images, an array of image objects
+  error, if there is one.
+  Pre-conditions:
   None
-  Post-conditions: 
+  Post-conditions:
   None
 */
 module.exports.getRandomImages= (function(count, callback) {
 
-    database.query("SELECT * FROM images ORDER BY RAND() LIMIT " + count, function(rows, error){
-	callback(rows, error);
-    });
+  filedatabase.query("SELECT * FROM images ORDER BY RAND() LIMIT " + count, function(rows, error){
+    callback(rows, error);
+  });
 
 });
 
@@ -39,24 +45,25 @@ module.exports.getRandomImages= (function(count, callback) {
  * Access random images in the gallery.
  */
 /*
-  Procedure: 
+  Procedure:
   gallery.getRecentImages(count, callback));
-  Purpose: 
+  Purpose:
   Grabs the most recent images from the image table and returns the information
-  Parameters: 
+  Parameters:
   count, how many images to return
   callback, a function describing what to do with the results
-  Produces: 
-  rows
-  Pre-conditions: 
+  Produces:
+  images, an array of image objects
+  error, if there is one.
+  Pre-conditions:
   None
-  Post-conditions: 
+  Post-conditions:
   None
 */
 module.exports.getRecentImages= (function(count, callback) {
 
-    database.query("SELECT * FROM images ORDER BY modifiedAt DESC LIMIT " + count, function(rows, error){
-	callback(rows, error);
-    });
+  filedatabase.query("SELECT * FROM images ORDER BY modifiedAt DESC LIMIT " + count, function(rows, error){
+    callback(rows, error);
+  });
 
 });
