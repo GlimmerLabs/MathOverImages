5eeb7a398142da0cb84ee84bab18b8bb1b13ef8a
alexthemitchell <mitchell17@grinnell.edu>
2014-06-19 13:46:04 -0500

    Add backup script, ignore backup files

---
+++ Site/database_maintenance/backup_database.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+# backup_database.sh
+# Alex Mitchell '17
+# Glimmer Labs Summer 2014
+
+# Generate datename
+filename=mist_backup_`date +"%Y_%m_%d"`.sql
+echo $filename
+# Dump Database
+/usr/local/mysql/bin/mysqldump -hglimmer.grinnell.edu -uroot -p mist -r$filename
+
+# Move Database to backups folder
+mv $filename database_backups/$filename
