cef9b5c26aaff2002681839e1242be247ba6f6ac
Samuel A. Rebelsky <rebelsky@grinnell.edu>
2014-06-25 15:48:51 -0500

    Clean up animator code.

--- Site/public/js/mistui-animator.js
+++ Site/public/js/mistui-animator.js
@@ -9,81 +9,59 @@
 
 // try { MIST: } catch (err) { MIST = {}; }
 if (!MIST.ui) { MIST.ui = {}; }
-// Declare constructor
-MIST.ui.Animator = function(on, exp, params, context, canvas, log){
-  // +-----------+-----------------------------------------------------
-  // | Animation |
-  // +-----------+
-  
-  /**
-   * The number of frames per second.
-   */
-  this.fps = 30;
-    
-  /**
-   * Are we animating our image?
-   */
-  this.on = on;
-  
-  /**
-   * The increments we use when simulating parameters.
-   */
-  this.increments = [19,23,29,31,37,41,43,47,53,59,61,67,71].map(function (val) { return val/3000; });
-  
-  /**
-   * When was the last time we drew a frame?  (Used primarily when we
-   * need to re-render a particular frame.)
-   */
-  this.time = { t:0, m:0 };
- 
+
+// +--------------+--------------------------------------------------
+// | Constructors |
+// +--------------+
+
+/**
+ * Create a new animator *without* rendering the frame.
+ */
+MIST.ui.Animator = function(exp, params, context, canvas, log) {
   // Remember parameters
   this.exp = exp;
   this.params = params;
   this.context = context;
   this.canvas = canvas;
-  this.log = log;
-}
-/**
- * Stop the animation from running.
- */
-MIST.ui.Animator.prototype.stopAnimation = function() {
-  this.on =false;
-} // stopAnimation
+  this.log = log || (function(txt) {});
 
-/**
- * Start the animation running.
- */
-MIST.ui.Animator.prototype.startAnimation = function()
-{
-  // Set up a hash for the parameters
-  this.parameters = {};
-  if (this.params != "") {
-    var tmp = this.params.split(",");
-    for (var i = 0; i < tmp.length; i++) {
-      if (this.params[i] != "") {
-        this.parameters[tmp[i]] = -1;
-      } // if
-    } // for
-  } // if (
+  // The number of frames per second.
+  this.fps = 30;
+   
+  // Are we animating the image?
+  this.on = true;
+  
+  // The increments used for variables.
+  this.increments = [19,23,29,31,37,41,43,47,53,59,61,67,71].map(function (val) { return val/3000; });
+  
+   // When was the last time we drew a frame?  (Used primarily when we
+   // need to re-render a particular frame.)
+  this.time = { t:0, m:0 };
+ 
+   // Set up the render width and height
+  if (exp.indexOf('t.') >= 0) {
+    this.renderWidth = 200;
+    this.renderHeight = 200;
+  }
+  else {
+    this.renderWidth = canvas.width;
+    this.renderHeight = canvas.height;
+  }
 
-  // Get the remaining info
+  // Parse the expression
   try {
     this.expParsed = MIST.parse(this.exp);
-    this.on = exp.indexOf("t.") > -1;       // HACK!
-    this.context = this.context;
-    this.canvas = this.canvas;
-    this.log = this.log;
-    this.frame();
   }
   catch (err) {
-    log(err);
+    this.log(err);
     throw err;
   }
-} // startAnimation
-// Function to change fps of animation(defaults to 30 if unspecified)
-Mist.ui.Animator.prototype.setFps = function(fps){
-  this.fps = fps || 30;
-}
+} // Constructor
+
+// +---------+-------------------------------------------------------
+// | Methods |
+// +---------+
+
 /**
  * Do one frame of the animation.
  */
@@ -99,19 +77,75 @@ MIST.ui.Animator.prototype.frame = function() {
     paramInfo += p + ": " + newval + "\n";
   } // for
   var context = this.context;
+
   // Inform the user
-  this.log(paramInfo);
+  if (paramInfo) {
+    this.log(paramInfo);
+  } // if (paramInfo)
+
   // Make the frame
-  if (this.on) {
-    this.time = MIST.render(this.expParsed, context,
-        this.canvas, 200, 200);
-  }
-  else {
-    this.time = MIST.render(this.expParsed, context,
-        this.canvas);
-  }
+  this.time = MIST.render(this.expParsed, this.context, this.canvas, 
+      this.renderWidth, this.renderHeight);
+} // frame
+
+/**
+ * Run the animation.
+ */
+MIST.ui.Animator.prototype.run = function() {
+  var runner = function(animator) { 
+    return function() { return animator.run(); }
+   }; 
+
+  // Build one frame
+  this.frame();
+
   // And schedule the next frame
   if (this.on) {
-    setTimeout(this.frame, 1000/this.fps);
+    var animator = this;
+    setTimeout(runner(this), 1000/this.fps);
   }
-} // MIST.ui.animator.frame()
+}; // run
+
+/**
+ * Change the fps of the animation.  (Defaults to 30 if unspecified.)
+ */
+MIST.ui.Animator.prototype.setFps = function(fps) {
+  this.fps = fps || 30;
+}; // setFps
+
+/**
+ * Set the resolution for rendering.
+ */
+MIST.ui.Animator.prototype.setResolution = function(width,height) {
+  this.renderWidth = width;
+  this.renderHeight = height;
+}; // setResolution
+
+/**
+ * Stop the animation from running.
+ */
+MIST.ui.Animator.prototype.stop = function() {
+  this.on = false;
+}; // stop
+
+/**
+ * Start the animation running.
+ */
+MIST.ui.Animator.prototype.start = function()
+{
+  // Set up a hash for the parameters
+  this.parameters = {};
+  if (this.params != "") {
+    var tmp = this.params.split(",");
+    for (var i = 0; i < tmp.length; i++) {
+      if (this.params[i] != "") {
+        this.parameters[tmp[i]] = -1;
+      } // if
+    } // for
+  } // if (this.params != "")
+
+  // Get the remaining info
+  this.on = this.exp.indexOf("t.") > -1;       // HACK!
+  this.run();
+}; // start
+
