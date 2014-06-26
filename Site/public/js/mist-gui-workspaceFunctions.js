0105006d432d502119940d80dc213275eaa552e2
Zoe Wolter <wolterzo@grinnell.edu>
2014-06-26 14:35:27 -0500

    added file workspaceFunctions
    workspaceFunctions contains meta functions for interactions between the
    server and the workspace

---
+++ Site/public/js/mist-gui-workspaceFunctions.js
@@ -0,0 +1,27 @@
+/* workspace functions */
+
+
+
+
+/**
+ * resetWorkspace removes all elements from the active workspace,
+ * and all records of 
+ */
+var resetWorkspace = function() {
+	//STUB
+}
+
+/**
+ * saveWorkspace saves the active workspace to the server.
+ */
+var saveWorkspace = function() {
+	// STUB
+}
+
+/**
+ * loadWorkspace loads a given workspace from the server onto the screen.
+ */
+var loadWorkspace = function(workspace) {
+	// STUB
+}
+
