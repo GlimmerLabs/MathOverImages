4183666548f27dab09927605785672772dfa1f3a
Daniel Rebelsky <drebelsky@gmail.com>
2014-06-27 11:23:58 -0500

    Fixed regular expression

--- Site/public/js/mist-gui-constructors.js
+++ Site/public/js/mist-gui-constructors.js
@@ -319,7 +319,7 @@ var createEditableText = function (group) {
 	group.add(editableTextBox);
 	editableTextBox.setEditable(true);
 	editableTextBox.aligned = "center";
-	editableTextBox.matchingCharacters = /^-?[0-9]+\.?[0-9]*$/;
+	editableTextBox.matchingCharacters = /^-?[0-9]*\.?[0-9]*$/;
 	editableTextBox.defaultText = 'Enter a Value';
 	editableTextBox.drawMethod = function(){
 		workLayer.draw()
