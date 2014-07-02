70b2e215bed10b44470bdffe2d179209a72ce26e
wolterzo <rebelsky@ma5666.local>
2014-07-02 14:08:12 -0500

    added layers to global variables

--- Site/public/js/mist-gui-globalVariables.js
+++ Site/public/js/mist-gui-globalVariables.js
@@ -139,3 +139,30 @@ var animation = false;
  * The offset in an operation node to the set of offsets.
  */
 var OUTLET_OFFSET = 3;
+
+
+/**
+ * Layers of the workspace:
+ * 1. The line layer holds connecting lines between nodes in the work area.
+ * 2. The menu layer holds the buttons that users can click on to drag a new node into
+ *    the work area.
+ * 3. The menuButton layer holds the super buttons that are used to expand the menus.
+ * 4. The toolboxLayer holds the draggable toolbox
+ * 5. The work layer holds all active nodes that are either connected or available to
+ *    be connected.
+ * 6. The border layer stores static elements of the page such as dividing lines.
+ * 7. The drag layer holds nodes while they are being moved about the workspace.
+ * 8. The text layer hold the editabe text boxes for constant values and funtion text.
+ */
+
+ var lineLayer = new Kinetic.Layer();
+ var menuLayer = new Kinetic.Layer();
+ var menuButtonLayer = new Kinetic.Layer();
+ var menuControlLayer = new Kinetic.Layer();
+ var workLayer = new Kinetic.Layer();
+ var borderLayer = new Kinetic.Layer();
+ var funBarLayer = new Kinetic.Layer();
+ var toolboxLayer = new Kinetic.Layer();
+ var dragLayer = new Kinetic.Layer();
+ var textLayer = new Kinetic.Layer();
+ var labelLayer = new Kinetic.Layer();
--- Site/public/js/mist-gui-initializeStage.js
+++ Site/public/js/mist-gui-initializeStage.js
@@ -5,32 +5,6 @@ var stage = new Kinetic.Stage({
   });
 
 /**
- * Layers of the workspace:
- * 1. The line layer holds connecting lines between nodes in the work area.
- * 2. The menu layer holds the buttons that users can click on to drag a new node into
- *    the work area.
- * 3. The menuButton layer holds the super buttons that are used to expand the menus.
- * 4. The toolboxLayer holds the draggable toolbox
- * 5. The work layer holds all active nodes that are either connected or available to
- *    be connected.
- * 6. The border layer stores static elements of the page such as dividing lines.
- * 7. The drag layer holds nodes while they are being moved about the workspace.
- * 8. The text layer hold the editabe text boxes for constant values and funtion text.
- */
-
- var lineLayer = new Kinetic.Layer();
- var menuLayer = new Kinetic.Layer();
- var menuButtonLayer = new Kinetic.Layer();
- var menuControlLayer = new Kinetic.Layer();
- var workLayer = new Kinetic.Layer();
- var borderLayer = new Kinetic.Layer();
- var funBarLayer = new Kinetic.Layer();
- var toolboxLayer = new Kinetic.Layer();
- var dragLayer = new Kinetic.Layer();
- var textLayer = new Kinetic.Layer();
- var labelLayer = new Kinetic.Layer();
-
-/**
  * Add the layers to the stage. The top layer is the one most recently added, and
  * objects within a layer follow the same logic.
  */
@@ -40,10 +14,10 @@ var stage = new Kinetic.Stage({
  stage.add(menuControlLayer);
  stage.add(workLayer);
  stage.add(borderLayer);
- stage.add(toolboxLayer);
  stage.add(funBarLayer);
- stage.add(dragLayer);
  stage.add(textLayer);
  stage.add(labelLayer);
+ stage.add(toolboxLayer);
+ stage.add(dragLayer);
 
 readyEditing(stage);
