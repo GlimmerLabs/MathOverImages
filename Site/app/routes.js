112a924170e48a4c6e3d9a41004808191f6e5a54
Victoria Tsou <victoria@glimmer.grinnell.edu>
2014-07-08 15:48:26 -0500

    Delete Entire Album and redirect to user's album page

--- Site/app/routes.js
+++ Site/app/routes.js
@@ -101,6 +101,10 @@ module.exports = function(app,passport,database) {
  if(req.body.deleteImage != null) {
     var albumContents = require("../functions/albumContents.js");
     albumContents.deleteFromAlbums(req, res, database);
+  }
+ else if (req.body.deleteWholeAlbum != null) {
+   var albumContents = require("../functions/albumContents.js");
+   albumContents.deleteAlbum(req, res, database);
   };
 });
 
