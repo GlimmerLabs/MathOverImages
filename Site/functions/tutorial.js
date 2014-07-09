7ed1dcd90cde15a3f3434069770a29d246586637
Samuel A. Rebelsky <rebelsky@grinnell.edu>
2014-07-08 22:39:51 -0500

    Generalize mechanism for generating tutorial pages.

--- Site/functions/tutorial.js
+++ Site/functions/tutorial.js
@@ -1,20 +1,39 @@
 /**
- * Functions related to the tutorial
+ * Functions related to the tutorial pages.
  */
 
-console.log("Loading tutorial library");
+// +--------------------+----------------------------------------------
+// | Exported Functions |
+// +--------------------+
+
+/**
+ * gui - Render a page in the GUI tutorial.
+ */
+module.exports.gui = function(req, res) {
+  var pages = ['start', 'values'];
+  tutorialPage(req, res, "/tutorial/gui/", req.params.page, pages);
+} // gui
 
 /**
  * intro - Render a page in the introductory tutorial.
  */
 module.exports.intro =  function(req, res) {
   // The list of pages in the introduction, in order
-  var pages = ['start', 'end'];
+  var pages = ['start', 'world', 'black', 'white', 'grey', 'x', 'y', 
+      'xtimesy', 'next'];
+  tutorialPage(req, res, "/tutorial/intro/", req.params.page, pages)
+} // intro
+
+// +-----------------+-------------------------------------------------
+// | Local Utilities |
+// +-----------------+
+
+var tutorialPage = function(req, res, path, page, pages) {
   // The name of the page and its index in the array
-  var page = res.params.page;
+  var page = req.params.page;
   var index = 0;
   // Compute the index and page name
-  if (isNan(page)) {
+  if (isNaN(page)) {
     index = pages.indexOf(page);
   }
   else {
@@ -26,17 +45,17 @@ module.exports.intro =  function(req, res) {
   var prev = undefined;
   var next = undefined;
   if (index > 0) {
-    prev = "/tutorial/intro/" + pages[index-1];
+    prev = path + pages[index-1];
   }
   if (index < pages.length-1) {
-    next = "/tutorial/intro/" + pages[index+1];
+    next = path + pages[index+1];
   }
 
   // Determine the local path to the view
-  var view = '../public/views/tutorial-intro/' + page;
+  var view = '../public/views' + path + page + '.jade';
+  console.log(view);
 
   // And render the page
   res.render(view, { next: next, prev: prev });
-}; // intro
+}; // tutorialPage
 
-introPages = {};
