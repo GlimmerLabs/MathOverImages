7dfe9f06637e68fd603f42582bbd0f3676b24b54
Daniel A. Rebelsky <drebelsky@gmail.com>
2014-07-10 14:37:40 -0500

    Fixed allImageinAlbum formatting

--- Site/functions/albums.js
+++ Site/functions/albums.js
@@ -1,7 +1,26 @@
 /**
  * Functions related to the album.
  */
-
+var setLikes = function(imageArray, userID, callback) {
+  var imageArrayClone = imageArray.slice(0, imageArray.length);
+  var errorsArray = [];
+  var likes = {counter: 1, likes: []};
+  for(var image = 0; image < imageArrayClone.length; image++) {
+    (function(currentIndex){
+      filedatabase.hasLiked(userID, imageArrayClone[currentIndex].imageid, function(liked, error) {
+        likes.likes[currentIndex] = liked;
+        likes.counter++;
+        if(likes.counter == imageArray.length) {
+          for(var i=0;i<likes.likes.length;i++) {
+            imageArrayClone[i].liked = likes.likes[i];
+          }
+          callback(imageArrayClone, errorsArray);
+        }
+        errorsArray.push(error);
+      });
+    })(image);
+  }
+}
 module.exports.buildPage =  function(req, res, database) {
 database.getIDforUsername(req.params.username,
 	function(userid, error) {
@@ -23,23 +42,23 @@ database.getIDforUsername(req.params.username,
 };
 
 module.exports.allImagesinAlbum = function(req, res, database) {
-database.getIDforUsername(req.params.username,
-	function(userid, error) {
-	    if(error)
-		res.end (error);
-	    else
-	database.getAllImagesforUser(userid, function(images, error){
-	    if(error)
-		res.end(error)
-	    else
-		res.render('../public/views/imagesCompilation.jade', {
-		    loggedIn: req.session.loggedIn,
-		    user:req.session.user,
-		    images:images,
-		    viewing:req.params.username
-		});
-	});
-});
+  database.getIDforUsername(req.params.username,
+    function(userid, error) {
+      if(error)
+        res.end (error);
+      else
+        database.getAllImagesforUser(userid, function(images, error){
+          if(error)
+            res.end(error)
+          else
+            res.render('../public/views/imagesCompilation.jade', {
+              loggedIn: req.session.loggedIn,
+              user:req.session.user,
+              images:images,
+              viewing:req.params.username
+            });
+        });
+  });
 };
 
 module.exports.createAlbum= function(req, res, database) {
