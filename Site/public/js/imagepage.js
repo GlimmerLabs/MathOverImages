10a8d6280705ca2bcab85ea3cae71ece60dd935d
Samuel A. Rebelsky <rebelsky@grinnell.edu>
2014-07-10 20:27:26 -0500

    Add support for embedding MIST images
    * /embed/imageid can be embedded with an iframe.
    * Update routes to use that URL
    * Add functions/embed.js and public/views/embed.jade to create the page
    * Update public/js/imagepage.js to expand the image to fill the page

--- Site/public/js/imagepage.js
+++ Site/public/js/imagepage.js
@@ -16,7 +16,44 @@
 var animator;
 
 $(document).ready(function() {
+  var canvas = document.getElementById("canvas");
+  if (canvas.className == "fullscreen") {
+    canvas.width = $(window).width() + 1;
+    canvas.height = $(window).height() + 1;
+    window.onresize = function(evt) {
+      console.log("resizing");
+      canvas.width = $(window).width();
+      canvas.height = $(window).height();
+      animator.width = canvas.width;
+      animator.height = canvas.height;
+    };
+  }
   animator = new MIST.ui.Animator(document.getElementById('code').innerHTML,
-    "", {}, document.getElementById("canvas"));
+    "", {}, canvas);
   animator.start();
+  document.body.onkeypress=function(evt) {
+    var code = evt.which || evt.keyCode || evt.charCode;
+    var char = String.fromCharCode(code);
+    console.log("Pressed '" + char + "'");
+    switch (char) {
+      case "s":
+        if (animator.on) {
+          animator.stop();
+        }
+        else {
+          animator.start();
+        }
+        break;
+      case "+":
+        animator.renderWidth = 
+            Math.min(animator.renderWidth+50, animator.width);
+        animator.renderHeight = 
+            Math.min(animator.renderHeight+50, animator.height);
+        break;
+      case "-":
+        animator.renderWidth = Math.max(animator.renderWidth-50, 50);
+        animator.renderHeight = Math.max(animator.renderHeight-50, 50);
+        break;
+    } // switch
+  }; // document.body.onkeypress
 });
