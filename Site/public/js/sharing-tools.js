698ac22ac3176efbc8609c25676a765f83443bb8
wolterzo <rebelsky@ma5666.local>
2014-07-11 11:06:10 -0500

    added google+ follow in footer

--- Site/public/js/sharing-tools.js
+++ Site/public/js/sharing-tools.js
@@ -25,3 +25,10 @@ window.fbAsyncInit = function() {
     fjs.parentNode.insertBefore(js,fjs);
   }}
 (document,"script","twitter-wjs");
+
+/** Google+ **/
+(function() {
+    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+    po.src = 'https://apis.google.com/js/platform.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+  })();
