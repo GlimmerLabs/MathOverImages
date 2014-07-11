e6c95beb142d8c1e698d95279adc16ae5539820b
Daniel A. Rebelsky <drebelsky@gmail.com>
2014-07-11 10:12:45 -0500

    Made sure to sanitize expression before evaluating it.

--- Site/public/js/mistui-animator.js
+++ Site/public/js/mistui-animator.js
@@ -43,6 +43,10 @@ MIST.ui.Animator = function(exp, params, context, canvas, log) {
 
   // Parse the expression
   try {
+    var validStrings = MIST.builtins.functions.keys().concat(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.', 't.s', 't.m', 't.h', 't.d', 'x', 'y', '(', ')', ',']);
+    console.log("expression:", this.exp, "validStrings:", validStrings);
+    this.exp = MIST.sanitize(MIST.createSanitizer.apply(null, validStrings), this.exp);
+    console.log("Expression post-sanitization: "+this.exp);
     this.expParsed = MIST.parse(this.exp);
   }
   catch (err) {
