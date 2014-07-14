c4b3d044ef38682bceca097c1d937515d6068c38
wheelere <earnestw@Earnests-MacBook-Air.local>
2014-07-14 12:00:47 -0500

    added code conventions
    added comments to if statements and fixed indentation for some functions

--- Site/public/js/mist-gui-workLayerEvents.js
+++ Site/public/js/mist-gui-workLayerEvents.js
@@ -57,25 +57,25 @@ There are 3 different modes:
           currLine.destroy();
           if (isOutlet(shape)) {
             shape.scale({ x: 1, y: 1 });
-          }
+          } // if the shape was an outlet
           makingLine = false;
-        }
+        } // if the target of the connection is the source, or forming the connection would cause a cycle
         else if (isOutlet(shape)) {
-        //check if outlet already has an input
-        outlet = shape;
-        }
+          //check if outlet already has an input
+          outlet = shape;
+        } // if the object an outlet
         else if (isFunction(parent)) {
           // find empty outlet
           if (parent.attrs.numInputs < parent.attrs.maxInputs) {
             var i = OUTLET_OFFSET
             while (parent.children[i].attrs.lineIn) {
-              i++
-            }
+              i++;
+            } // skip past outlets with inputs
             outlet = parent.children[i];
-          }
+          } // if there are fewer than the maximum number of inputs
           else if (parent.attrs.maxInputs == 1) {
             outlet = parent.children[OUTLET_OFFSET];
-          }
+          } // if the max inputs for the function group is one
         } // else if function
         if (outlet) {
           var isReplacement;
@@ -86,7 +86,7 @@ There are 3 different modes:
             oldLine = source.attrs.lineOut[index];
             isReplacement = true;
             removeLine(oldLine);
-          } 
+          } // if outlet exists
           else {
             isReplacement = false;
           } // check if theres already a line going in to the outlet
@@ -105,67 +105,67 @@ There are 3 different modes:
             addOutlet(parent);
             if (parent.children[2].attrs.expanded) {
               renderCanvas(parent);
-            }
-          }
+            } // if the value/function has an expanded imageBox
+          } // if there isn't an open outlet and there should be
           insertToTable(currLine);
           if (!isReplacement) {
             insertToArray(actionToObject('insert', currLine));
-          }
+          } // if added connection is not a replacement
           else {
             insertToArray(actionToObject('replace', currLine, oldLine));
-          }
+          } // else its a replacement
           //setOutletOpacity(parent);
           updateForward(parent);
-        }  
+        } // if the outlet exists  
     } // if makingline
     else {
       if (isImageBox(shape)) {
         if (!shape.attrs.expanded) {
           renderCanvas(parent);
           shape.attrs.expanded = true;
-        } 
+        } // if the image box is not expanded
         else {
           shape.attrs.expanded = false;
           animation = false;
-          setTimeout(function() {collapseCanvas(parent)}, 50);
-        }
-        setTimeout(function() {workLayer.draw()}, 50);
-      } 
+          setTimeout(function() { collapseCanvas(parent) }, 50);
+        } // else the image box is expanded
+        setTimeout(function() { workLayer.draw() }, 50);
+      } // if the object is an image box
       else if (parent.name() != 'rgb') {
         makingLine = true;
         currLine = makeLine(parent);
         parent.attrs.lineOut[parent.attrs.lineOut.length] = currLine;
         lineLayer.add(currLine);
-      }
-    }
-  } 
+      } // if the object is part of an rgb 
+    } // else not making line
+  } // if line tool enabled 
   else if (deleteToolOn) {
     insertToArray(actionToObject('delete', parent));
     // deal with lines coming in to the node being deleted
     var targetLine;
-    for(var i = OUTLET_OFFSET; i < parent.children.length; i++) {
+    for (var i = OUTLET_OFFSET; i < parent.children.length; i++) {
       targetLine = parent.children[i].attrs.lineIn;
-      if (targetLine){
+      if (targetLine) {
         removeLine(targetLine);
-      }
-    }
-    // deal with the lines leading out of the node being deleted
+      } // if the lineIn exists
+    } // for every outlet, delete the line going into it
+
+    // deal with the lines leading out of the node to be deleted
     var lineOutLength = parent.attrs.lineOut.length;
-    for(var i = 0; i < lineOutLength; i++) {
+    for (var i = 0; i < lineOutLength; i++) {
       targetLine = parent.attrs.lineOut[0];
       removeLine(targetLine);
-    }
+    } // 
     var render = parent.attrs.renderLayer
     if (render != null) {
       render.destroy();
-    }
+    } // if the renderLayer exists
     if (currShape == parent) {
       currShape = null;
       updateFunBar();
-    }
-
+    } // if the object belongs to the global currShape
     parent.remove();
-  }
+  } // if the delete tool is enabled
   workLayer.draw();
   lineLayer.draw();
 });
@@ -205,7 +205,7 @@ There are 3 different modes:
       currLine.points()[2] = stage.getPointerPosition().x;
       currLine.points()[3] = stage.getPointerPosition().y;
       lineLayer.draw();
-    }
+    } // if makingLine
   });
 
   
@@ -223,28 +223,29 @@ workLayer.on('mouseover', function(evt) {
         renderCanvas(shape.getParent());
         if (animation) {
           setTimeout(frame, 50);
-        }
+        } // if animation is still enabled
       }
       frame();
-    }
+    } // if the mouseover object is an expanded imageBox
     if (makingLine) {
       var outlet;
       if (isOutlet(shape)) {
         outlet = shape;
-      }
+      } // if the mouseover object is an outlet
       else if (isFunction(parent)) {
+
         // find empty outlet
         if (parent.attrs.numInputs < parent.attrs.maxInputs) {
           var i = OUTLET_OFFSET
           while (parent.children[i].attrs.lineIn) {
-            i++
-          }
+            i++;
+          } // while the outlet at index i has a lineIn
           outlet = parent.children[i];
-        }
+        } // if the number of inputs is less than the max inputs
         else if (parent.attrs.maxInputs == 1) {
           outlet = parent.children[OUTLET_OFFSET];
-        }
-      }
+        } // if the function can only have 1 input
+      } // if the mouseover object is part of a function
       if (outlet && outlet.parent != currLine.attrs.source) {
         outlet.scale({
           x: 1.5,
@@ -257,109 +258,115 @@ workLayer.on('mouseover', function(evt) {
             shadowEnabled: true
           });
           lineLayer.draw();
-        }
+        } // if line exists
         workLayer.draw();
       } // if outlet
     } // if makingLine
-    } 
-    else if (deleteToolOn) {
-      if (isFunction(parent) || isValue(parent)) {
-        parent.children[0].setAttrs({
+  } // if the work tool or line tool is enabled
+  else if (deleteToolOn) {
+    if (isFunction(parent) || isValue(parent)) {
+      parent.children[0].setAttrs({
+        shadowColor: deleteColor,
+        shadowOpacity: 1,
+        shadowEnabled: true
+      });
+
+      // for all lines going out
+      var lineOut = parent.attrs.lineOut;
+      for (var i = 0; i < lineOut.length; i++) {
+        lineOut[i].setAttrs({
           shadowColor: deleteColor,
-          shadowOpacity: 1,
           shadowEnabled: true
         });
-        // for all lines going out
-        var lineOut = parent.attrs.lineOut;
-        for (var i = 0; i < lineOut.length; i++) {
-          lineOut[i].setAttrs({
+      } // for lineOut array
+
+      // for all lines coming in
+      var children = parent.children;
+      for (var i = OUTLET_OFFSET; i < children.length; i++) {
+        line = children[i].attrs.lineIn;
+        if (line) {
+          line.setAttrs({
             shadowColor: deleteColor,
             shadowEnabled: true
           });
-        } // for lineOut
-        //for all lines coming in
-        var children = parent.children;
-        for (var i = OUTLET_OFFSET; i < children.length; i++) {
-          line = children[i].attrs.lineIn;
-          if (line) {
-            line.setAttrs({
-              shadowColor: deleteColor,
-              shadowEnabled: true
-            });
-          } // if line
-        } // for incoming lines
-      }
-      workLayer.draw();
-      lineLayer.draw();
-    }
-  });
+        } // if line
+      } // for incoming lines
+    } // if the mouseover object belongs to a value 
+    workLayer.draw();
+    lineLayer.draw();
+  } // if the delete tool is enabled
+});
 
-/*
-  when the cursor is moved out of an outlet while drawing a line, return it to its
-  original size
-  */
-  workLayer.on('mouseout', function(evt) {
-    var shape = evt.target;
-    var parent = shape.getParent();
-    if (workToolOn || lineToolOn) {
-      if (isImageBox(shape) && shape.attrs.expanded) {
-        animation = false;
-      }
-      if (makingLine) {
-        var outlet;
-        if (isOutlet(shape)) {
-          outlet = shape;
-        }
-        else if (isFunction(parent)) {
-          // find empty outlet
-          if (parent.attrs.numInputs < parent.attrs.maxInputs) {
-            var i = OUTLET_OFFSET
-            while (parent.children[i].attrs.lineIn) {
-              i++
-            }
-            outlet = parent.children[i];
-          }
-          else if (parent.attrs.maxInputs == 1) {
-            outlet = parent.children[OUTLET_OFFSET];
-          }
-        }
-        if (outlet && outlet.parent != currLine.attrs.source) {
-          outlet.scale({
-            x: 1,
-            y: 1
-          });
-          line = outlet.attrs.lineIn;
-          if (line) {
-            line.setAttr('shadowEnabled', false);
-            lineLayer.draw();
-          }
-          workLayer.draw();
-        } // if outlet
-      }
-    } 
-    else if (deleteToolOn) {
-      if (isFunction(parent) || isValue(parent)) {
-        if (parent == currShape){
-          setSelectedShadow(parent);
-        } 
-        else {
-          removeShadow(parent);
-        }
-        // for all lines going out
-        var lineOut = parent.attrs.lineOut;
-        for (var i = 0; i < lineOut.length; i++) {
-          lineOut[i].setAttr('shadowEnabled', false);
-        } // for lineOut
-        //for all lines coming in
-        var children = parent.children;
-        for (var i = OUTLET_OFFSET; i < children.length; i++) {
-          line = children[i].attrs.lineIn;
-          if (line) {
-            line.setAttr('shadowEnabled', false);
-          } // if line
-        } // for incoming lines
-        }
+/**
+ * When the cursor is moved out of an outlet while drawing a line, return the outlet to its
+ * original size/shadow/state
+ */
+workLayer.on('mouseout', function(evt) {
+  var shape = evt.target;
+  var parent = shape.getParent();
+  if (workToolOn || lineToolOn) {
+    if (isImageBox(shape) && shape.attrs.expanded) {
+      animation = false;
+    } // if the mouseout object is an expanded imageBox
+    if (makingLine) {
+      var outlet;
+      if (isOutlet(shape)) {
+        outlet = shape;
+      } // if the shape is an outlet
+      else if (isFunction(parent)) {
+        // find the first empty outlet
+        if (parent.attrs.numInputs < parent.attrs.maxInputs) {
+          var i = OUTLET_OFFSET
+          while (parent.children[i].attrs.lineIn) {
+            i++
+          } // while the outlet at index i has a line in, skip forward
+          outlet = parent.children[i];
+        } // if the number of inputs is less than the max inputs
+        else if (parent.attrs.maxInputs == 1) {
+          outlet = parent.children[OUTLET_OFFSET];
+        } // if the function can only have 1 input
+      } // if the mouseover object belongs to a function
+      if (outlet && outlet.parent != currLine.attrs.source) {
+        outlet.scale({
+          x: 1,
+          y: 1
+        });
+        line = outlet.attrs.lineIn;
+        if (line) {
+          line.setAttr('shadowEnabled', false);
+          lineLayer.draw();
+        } // if line exists
         workLayer.draw();
-        lineLayer.draw();
-      }
-    });
+      } // if outlet exists and does not belong to the line's source
+    } // if makingLine
+  } // if the work tool or line tool is enabled 
+  else if (deleteToolOn) {
+    if (isFunction(parent) || isValue(parent)) {
+
+      // deal with shadows on the shape
+      if (parent == currShape) {
+        setSelectedShadow(parent);
+      } // if mouseout shape is the global currShape
+      else {
+        removeShadow(parent);
+      } // else its not the currShape
+
+      // deal with lines coming out of the shape
+      var lineOut = parent.attrs.lineOut;
+      for (var i = 0; i < lineOut.length; i++) {
+        lineOut[i].setAttr('shadowEnabled', false);
+      } // for lineOut array
+
+      // deal with lines going in to the shape
+      var children = parent.children;
+      for (var i = OUTLET_OFFSET; i < children.length; i++) {
+        line = children[i].attrs.lineIn;
+        if (line) {
+          line.setAttr('shadowEnabled', false);
+        } // if line in exists
+      } // for incoming lines
+    } // if the mouseover object belongs to a value or function
+    workLayer.draw();
+    lineLayer.draw();
+  }
+});
