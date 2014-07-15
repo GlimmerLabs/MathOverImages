38ee93dbb5bd777586e43b7fae959e183a799f14
alexthemitchell <mitchell17@grinnell.edu>
2014-07-15 16:16:33 -0500

    Add mailing functionality

--- Site/functions/mail.js
+++ Site/functions/mail.js
@@ -1,4 +1,5 @@
 var validate = require('validator');
+var database = require('./database');
 
 // Thanks http://www.davidpirek.com/blog/run-shell-from-nodejs-function for the runShell command
 var runShell = function(command){
@@ -16,8 +17,18 @@ var runShell = function(command){
 
 
 // Example.
-module.exports.sendMail = (function(to, subject, message){
+var sendMail = (function(to, subject, message){
+  message = message.replace("'", "\'")
   runShell("echo '" + message + "' | mailx -a 'Content-type: text/html;' -s '" + subject + "' " + to);
 });
 
+var validateEmail = (function(user){
+  sendMail(user.email, user.forename + ", validate your email address for MIST", "<a href='http://glimmer.grinnell.edu'>Click Here to validate your address</a>")
+});
 
+var alertComment = (function(toUser, imageid, commentid){
+  console.log("validateEmail");
+  sendMail(user.email, user.forename + ", validate your email address for MIST", "<a href='http://glimmer.grinnell.edu'>Click Here to validate your address</a>")
+});
+
+module.exports.validateEmail = validateEmail;
