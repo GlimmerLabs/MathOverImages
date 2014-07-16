4496446756b2413931c2d7da88f8a12536da9bd5
Daniel A. Rebelsky <drebelsky@gmail.com>
2014-07-16 14:32:16 -0500

    Changed onkeydown to onkeyup, so it behaves more predictably. Previously with onkeydown and onkeypress, the event got called before or after the text changed, so there was no way to properly support it on all browsers.

--- Site/public/js/main.js
+++ Site/public/js/main.js
@@ -2,11 +2,8 @@ $(document).ready(function(){
   // Search functions
   var searchResults = document.getElementById("searchResults");
   var searchBar = document.getElementById("searchBar");
-  searchBar.onkeydown = function(e) {
-    var keycode = e.which || e.keyCode || e.charCode;
-    var deleteKeyPressed = keycode == 46 || keycode == 8;
-    if((this.value.length >= 2 && !deleteKeyPressed) || (this.value.length > 3)) {
-      console.log(this.value, this.value.length);
+  searchBar.onkeyup = function(e) {
+    if(this.value.length >= 3) {
       omnisearch(this.value, createNewMenu)
     }
     else {
