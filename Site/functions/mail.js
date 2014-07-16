c6aaa863c9b102a66fc0ac3a5c185b261fd30147
alexthemitchell <mitchell17@grinnell.edu>
2014-07-16 11:08:32 -0500

    Add documenation for mail

--- Site/functions/mail.js
+++ Site/functions/mail.js
@@ -1,6 +1,21 @@
+/**
+* mail.js
+* Send email and other mail related services
+*/
+
 var validate = require('validator');
 var database = require('./database');
 var fs = require('fs');
+
+/**
+* enterUserInfo replaces templating in HTML files with actual values.
+*/
+var enterUserInfo = (function(user, template) {
+  return (template.replace(/(?:{{ FORENAME }})/g, user.forename)
+          .replace(/(?:{{ SURNAME }})/g, user.surname)
+          .replace(/(?:{{ EMAIL }})/g, user.email)
+          .replace(/(?:{{ USERNAME }})/g, user.username))
+});
 
 // Thanks http://www.davidpirek.com/blog/run-shell-from-nodejs-function for the runShell command
 var runShell = function(command){
@@ -15,30 +30,33 @@ var runShell = function(command){
   exec(command, puts);
 }
 
+/**
+* sendMail executes the actual send command. As it uses mailx, the "from" header will be based on its configuration
+*/
 var sendMail = (function(to, subject, message){
   message = message.replace(/\"/g, '\\"').replace(/!/g, "&#33;");
   console.log('echo "' + message + '" | mailx -a "Content-type: text/html;" -s "' + subject + '" ' + to);
   runShell('echo "' + message + '" | mailx -a "Content-type: text/html;" -s "' + subject + '" ' + to);
 });
 
+/**
+* validateEmail sends an email to a user about how they may verify their email address.
+*/
 var validateEmail = (function(user){
   fs.readFile("public/emails/validateEmail.html", function (error, data){
     if (error){
       console.log(error);
     }
     else {
+      var link = "http://glimmer.grinnell.edu/verify?id=" + user.userid +"&token=" + "dog";
       var email = data.toString();
-      email = enterUserInfo(user, email).replace(/(?:{{ LINK }})/g, 'http://www.google.com');
+      email = (enterUserInfo(user, email)
+               .replace(/(?:{{ LINK }})/g, link));
       sendMail(user.email, user.forename + ", validate your email address for MIST", email);
     }
   });
 });
 
-var enterUserInfo = (function(user, template) {
-  return (template.replace(/(?:{{ FORENAME }})/g, user.forename)
-          .replace(/(?:{{ SURNAME }})/g, user.surname)
-          .replace(/(?:{{ EMAIL }})/g, user.email)
-          .replace(/(?:{{ USERNAME }})/g, user.username))
-});
+
 
 module.exports.validateEmail = validateEmail;
