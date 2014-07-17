96851f506c072c3c1a5241a92abb5647009586c7
Amanda Hinchman-Dominguez <hinchman17@grinnell.edu>
2014-07-17 14:03:39 -0500

    Preliminary styling of challenge page and submission functions

--- Site/functions/challenge.js
+++ Site/functions/challenge.js
@@ -164,3 +164,34 @@ module.exports.view = function(req, res, database) {
   });
 }; // view
 
+module.exports.submission = function(req, res, database) {
+//img1 - challenge
+//img2 - submission
+  var w1=img1.width;
+  var h1=img1.height;
+  var pixelData1=canvas.getContext('2d').getImageData(i, j, w1, h1);
+  var w2=img2.width;
+  var h2=img2.height;
+  var pixelData1=canvas.getContext('2d').getImageData(i, j, w2, h2);
+  var similarity=0;
+  for (var i=0; i<pixelData1.length; i+=4)
+  {
+    var pixel1=[pixelData1[i], pixelData1[i+1], pixelData1[i+2]];
+    var pixel2=[pixelData2[i], pixelData2[i+1], pixelData2[i+2]];
+    var diffR=abs(pixel2[0]-pixel1[0]);
+    var diffG=abs(pixel2[1]-pixel1[1]);
+    var diffB=abs(pixel2[2]-pixel1[2]);
+    if (diffR < .01 && diffG < .01 && diffB < .01)
+    {
+      similarity++;
+    }
+  }
+  if (similarity >= (pixelData1.length*.9))
+  {
+    return 1;
+  }
+  else
+  {
+    return 0;
+  }
+};
