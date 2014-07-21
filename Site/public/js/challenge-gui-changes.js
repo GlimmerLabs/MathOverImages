22ec308e6b14096407ff422f72904e243256b3ab
Amanda Hinchman-Dominguez <hinchman17@grinnell.edu>
2014-07-21 16:30:00 -0500

    Challenges now works for exact code, checks if logged in for submits, rellocates to challenges page if correct

--- Site/public/js/challenge-gui-changes.js
+++ Site/public/js/challenge-gui-changes.js
@@ -14,14 +14,17 @@ var submitChallenge = function() {
   var i = url.length;
   while (i > 0 && url[--i] != '/');
   var id = url.substring(i+1);
-  
+  console.log(id);
   var request = new XMLHttpRequest();
   var data = "action=submitchallenge&code=" + currText + "&id=" + id;
+  console.log(data);
   request.open("POST", "/api", false);
   request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
   request.send(data);
+  console.log(request.responseText);
+  console.log(eval(request.responseText));
   return eval(request.responseText);
-}
+} 
 
 
 /** add button for submitting challenges **/
@@ -86,55 +89,57 @@ submitChallGroup.on('mousedown', function() {
 submitChallGroup.on('mouseup', function() {
   submitChallRect.setAttr('shadowEnabled', false);
   var submissionCode = submitChallenge();
-  var id = database.sanitize(req.params.id); // challenge
-  var query = "SELECT code FROM challenges WHERE id=" + id + ";";
-  database.query(query, function(rows, error) {
-    if (error) {
-      res.send(error);
-      return;
-    } // if error
-    var challengeCode = MIST.sanitize(builtinsPattern, rows[0]['code']);
-    var challengeParsed=MIST.parse(challengeCode);
-    var submissionParsed=MIST.parse(submissionCode);
-    var d = new Date();
-    var t = {
-      s: d.getMilliseconds()/500 - 1,
-      m: (d.getSeconds()*1000 + d.getMilliseconds())/30000 - 1,
-      h: (d.getMinutes()*60 + d.getSeconds())/1800 - 1,
-      d: (d.getHours()*60 + d.getMinutes())/720 - 1
-    };
-    var m = {
-      x: MIST.mouseX,
-      y: MIST.mouseY,
-      X: MIST.clickX,
-      Y: MIST.clickY
-    };
-    var fun1 = MIST.expToRGB("untitled image", challengeParsed, context);
-    var fun2 = MIST.expToRGB("untitled image", submissionParsed, context);
-    var similarity=0;
-    for (var x=0; x<200; x+=4) //y
+  if (submissionCode)
+  {
+    alert("Correct! You've completed the challenge!");
+    window.location.href = '../';
+    //credit to http://stackoverflow.com/questions/19825283/redirect-to-a-page-url-after-alert-button-is-pressed
+  }
+/*  var challengeCode = document.getElementById("code");
+  var context = {};
+  var d = new Date();
+  var t = {
+    s: d.getMilliseconds()/500 - 1,
+    m: (d.getSeconds()*1000 + d.getMilliseconds())/30000 - 1,
+    h: (d.getMinutes()*60 + d.getSeconds())/1800 - 1,
+    d: (d.getHours()*60 + d.getMinutes())/720 - 1
+  };
+  var m = {
+    x: MIST.mouseX,
+    y: MIST.mouseY,
+    X: MIST.clickX,
+    Y: MIST.clickY
+  };
+  console.log(challengeCode);
+  console.log(submissionCode);
+  var fun1 = MIST.expToRGB("untitled image", challengeCode, context);
+  var fun2 = MIST.expToRGB("untitled image", submissionCode, context);
+  var similarity=0;
+  for (var x=0; x<200; x+=4) //y
+  {
+    for (var y=0; y<200; y+=4) //x
     {
-      for (var y=0; y<200; y+=4) //x
+      var rgb1 = fun1(x,y,t,m);
+      var rgb2 = fun2(x,y,t,m);
+      var diffR=abs(rgb2[0]-rgb1[0]);
+      var diffG=abs(rgb2[1]-rgb1[1]);
+      var diffB=abs(rgb2[2]-rgb1[2]);
+      if (diffR < .01 && diffG < .01 && diffB < .01)
       {
-        var rgb1 = fun1(x,y,t,m);
-        var rgb2 = fun2(x,y,t,m);
-        var diffR=abs(rgb2[0]-rgb1[0]);
-        var diffG=abs(rgb2[1]-rgb1[1]);
-        var diffB=abs(rgb2[2]-rgb1[2]);
-        if (diffR < .01 && diffG < .01 && diffB < .01)
-        {
-          similarity++;
-        }
+        similarity++;
       }
     }
-// NEED TO INCLUDE mist-render.js and mistui-animator.js
+  }
   if (similarity >= 9000) {
     alert("Correct! You've completed the challenge!");
   } // if correct
   else {
-    // alert("That's not quite right. Make sure the selected image matches the one on the left.")
-    alert("Sorry, submitting solutions is not yet implemented.");
+    alert("That's not quite right. Make sure the selected image matches the one on the left.")
   } // else incorrect
-  }
+}*/
+else
+{
+  alert("That's not quite right. Make sure the selected image matches the one on the left.")
+}
   funBarLayer.draw();
 });
