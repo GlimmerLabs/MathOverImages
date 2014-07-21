63b04e5fd4ebd801a4f9a2cc3ecad35170932a8e
Amanda Hinchman-Dominguez <hinchman17@grinnell.edu>
2014-07-21 13:43:17 -0500

    Don't look at these they don't work :(

--- Site/public/js/challenge-gui-changes.js
+++ Site/public/js/challenge-gui-changes.js
@@ -85,13 +85,55 @@ submitChallGroup.on('mousedown', function() {
 
 submitChallGroup.on('mouseup', function() {
   submitChallRect.setAttr('shadowEnabled', false);
-  var correct = submitChallenge();
-  // WHAT DO WE WANT TO DO WITH THIS INFORMATION:
-  if (correct) {
+  var submissionCode = submitChallenge();
+  var id = database.sanitize(req.params.id); // challenge
+  var query = "SELECT code FROM challenges WHERE id=" + id + ";";
+  database.query(query, function(rows, error) {
+    if (error) {
+      res.send(error);
+      return;
+    } // if error
+    var challengeCode = MIST.sanitize(builtinsPattern, rows[0]['code']);
+    var challengeParsed=MIST.parse(challengeCode);
+    var submissionParsed=MIST.parse(submissionCode);
+    var d = new Date();
+    var t = {
+      s: d.getMilliseconds()/500 - 1,
+      m: (d.getSeconds()*1000 + d.getMilliseconds())/30000 - 1,
+      h: (d.getMinutes()*60 + d.getSeconds())/1800 - 1,
+      d: (d.getHours()*60 + d.getMinutes())/720 - 1
+    };
+    var m = {
+      x: MIST.mouseX,
+      y: MIST.mouseY,
+      X: MIST.clickX,
+      Y: MIST.clickY
+    };
+    var fun1 = MIST.expToRGB("untitled image", challengeParsed, context);
+    var fun2 = MIST.expToRGB("untitled image", submissionParsed, context);
+    var similarity=0;
+    for (var x=0; x<200; x+=4) //y
+    {
+      for (var y=0; y<200; y+=4) //x
+      {
+        var rgb1 = fun1(x,y,t,m);
+        var rgb2 = fun2(x,y,t,m);
+        var diffR=abs(rgb2[0]-rgb1[0]);
+        var diffG=abs(rgb2[1]-rgb1[1]);
+        var diffB=abs(rgb2[2]-rgb1[2]);
+        if (diffR < .01 && diffG < .01 && diffB < .01)
+        {
+          similarity++;
+        }
+      }
+    }
+// NEED TO INCLUDE mist-render.js and mistui-animator.js
+  if (similarity >= 9000) {
     alert("Correct! You've completed the challenge!");
   } // if correct
   else {
     alert("That's not quite right. Make sure the selected image matches the one on the left!")
   } // else incorrect
+  }
   funBarLayer.draw();
 });
