6cb76068f422808ac29b3b7b3c38778d90eb175b
Samuel A. Rebelsky <rebelsky@grinnell.edu>
2014-07-22 14:45:30 -0500

    Minor comment fix.

--- Site/public/js/challenge-gui-width.js
+++ Site/public/js/challenge-gui-width.js
@@ -1,3 +1,7 @@
-// Window width
+/**
+ * challenge-gui-width.js
+ *   Set the width of the GUI.  (Current design requires that we set that
+ *   width before loading any of the mist-gui libraries.)
+ */
 
-var width = 800
+var width = 800;
