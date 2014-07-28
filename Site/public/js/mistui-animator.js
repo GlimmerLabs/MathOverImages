4b3e2f83cd6f071be1185deb8c7912ee8ae7c36e
Daniel A. Rebelsky <drebelsky@gmail.com>
2014-07-28 13:29:04 -0500

    Compressed builtinsPattern

--- Site/public/js/mistui-animator.js
+++ Site/public/js/mistui-animator.js
@@ -18,7 +18,7 @@ if (!MIST.ui) { MIST.ui = {}; }
  * The pattern to identify builtins (or lack thereof).  Used for the
  * simple validation strategy.
  */
-var builtinsPattern = /(?:abs)|(?:avg)|(?:cos)|(?:mult)|(?:rgb)|(?:sign)|(?:neg)|(?:signz)|(?:sin)|(?:square)|(?:sum)|(?:wsum)|(?:null)|[0-9xy().,\-]|(?:t.s)|(?:t.m)|(?:t.h)|(?:t.d)|(?:m.x)|(?:m.y)|(?:mistif)/g
+var builtinsPattern = /(?:abs|avg|cos|mult|rgb|sign|neg|signz|sin|square|sum|wsum|null|mistif|t.s|t.m|t.h|t.d|m.x|m.y)|[0-9xy().,\-]/g
 /**
  * Adding backwards compatibilty for request animation frame.
  */
