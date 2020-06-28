2ad4db1a9a7f5964d6326bf17ae124f027b1c94f
Daniel Rebelsky <drebelsky@gmail.com>
2020-06-27 19:12:23 -0500

    Made funBarEvents a module

--- Site/public/react/js/mist-gui/funBarEvents.js
+++ Site/public/react/js/mist-gui/funBarEvents.js
@@ -1,32 +1,42 @@
-funBarSaveImGroup.on('mouseup', function(){
-  if (state.currShape && predicate.isRenderable(state.currShape)) {
-    utility.enableWorkTool();
-    openSavePopUp();
-    funBarSaveImGroup.children[0].setAttr('shadowEnabled', false);
-    layers.funBar.draw();
-  }
-});
+export default function(
+  funBarSaveImGroup,
+  state,
+  isRenderable,
+  enableWorkTool,
+  openSavePopUp,
+  funBarLayer,
+  valueStyle,
+) {
+  funBarSaveImGroup.on('mouseup', function(){
+    if (state.currShape && isRenderable(state.currShape)) {
+      enableWorkTool();
+      openSavePopUp();
+      funBarSaveImGroup.children[0].setAttr('shadowEnabled', false);
+      funBarLayer.draw();
+    }
+  });
 
-funBarSaveImGroup.on('mousedown', function(){
-  if (state.currShape && predicate.isRenderable(state.currShape)) {
-    funBarSaveImGroup.children[0].setAttr('shadowEnabled', true);
-    layers.funBar.draw();
-  }
-});
+  funBarSaveImGroup.on('mousedown', function(){
+    if (state.currShape && isRenderable(state.currShape)) {
+      funBarSaveImGroup.children[0].setAttr('shadowEnabled', true);
+      funBarLayer.draw();
+    }
+  });
 
-funBarSaveImGroup.on('mouseover', function(){
-  if (state.currShape && predicate.isRenderable(state.currShape)) {
-    funBarSaveImGroup.children[0].setAttr('fill', valueStyle.menuColorLight);
-    layers.funBar.draw();
-  }
-});
+  funBarSaveImGroup.on('mouseover', function(){
+    if (state.currShape && isRenderable(state.currShape)) {
+      funBarSaveImGroup.children[0].setAttr('fill', valueStyle.menuColorLight);
+      funBarLayer.draw();
+    }
+  });
 
-funBarSaveImGroup.on('mouseout', function(){
-  if (state.currShape && predicate.isRenderable(state.currShape)) {
-    funBarSaveImGroup.children[0].setAttrs({
-      fill: valueStyle.menuColor,
-      shadowEnabled: false
-    });
-    layers.funBar.draw();
-  }
-});
+  funBarSaveImGroup.on('mouseout', function(){
+    if (state.currShape && isRenderable(state.currShape)) {
+      funBarSaveImGroup.children[0].setAttrs({
+        fill: valueStyle.menuColor,
+        shadowEnabled: false
+      });
+      funBarLayer.draw();
+    }
+  });
+};
---
+++ Site/public/react/js/mistgui.js
@@ -0,0 +1,10 @@
+import createFunBarListeners from './mist-gui/funBarEvents.js';
+createFunBarListeners(
+  funBarSaveImGroup,
+  state,
+  predicate.isRenderable,
+  utility.enableWorkTool,
+  openSavePopUp,
+  layers.funBar,
+  valueStyle
+);
