d36888da514c2c0dee558948cb5d643d8d6483fc
Daniel Rebelsky <drebelsky@gmail.com>
2020-07-09 14:46:32 -0500

    Removed unnescesary DOM element creation in renderAt

--- Site/public/js/mist-render.js
+++ Site/public/js/mist-render.js
@@ -186,101 +186,95 @@ MIST.renderGIF = function(d, exp, context, canvas, renderWidth, renderHeight,
 
 /**
  * Render an expression at a particular time.
+ * Note that renderAt is wrapped in a IIFE in order to encapsulate the buffer canvas
  */
-MIST.renderAt = function(t, exp, context, canvas, 
+MIST.renderAt = (function() {
+  const buffer = document.createElement("canvas");
+  return function(t, exp, context, canvas, 
     renderWidth, renderHeight, imgLeft, imgTop, imgWidth, imgHeight) {
-  // Make sure that we have bounds.
-  if (!imgLeft) { imgLeft = 0; }
-  if (!imgTop) { imgTop = 0; }
-  if (!imgWidth) { imgWidth = canvas.width - imgLeft; }
-  if (!imgHeight) { imgHeight = canvas.height - imgTop; }
-  if (!renderWidth) { renderWidth = imgWidth; }
-  if (!renderHeight) { renderHeight = imgHeight; }
-  if (renderWidth > imgWidth) { renderWidth = imgWidth; }
-  if (renderHeight > imgHeight) { renderHeight = imgHeight; }
+    // Make sure that we have bounds.
+    if (!imgLeft) { imgLeft = 0; }
+    if (!imgTop) { imgTop = 0; }
+    if (!imgWidth) { imgWidth = canvas.width - imgLeft; }
+    if (!imgHeight) { imgHeight = canvas.height - imgTop; }
+    if (!renderWidth) { renderWidth = imgWidth; }
+    if (!renderHeight) { renderHeight = imgHeight; }
+    if (renderWidth > imgWidth) { renderWidth = imgWidth; }
+    if (renderHeight > imgHeight) { renderHeight = imgHeight; }
 
-  // Make sure that the rendering width and height are whole numbers
-  renderWidth = Math.round(renderWidth);
-  renderHeight = Math.round(renderHeight);
+    // Make sure that the rendering width and height are whole numbers
+    renderWidth = Math.round(renderWidth);
+    renderHeight = Math.round(renderHeight);
 
-  // Grab the canvas buffer.
-  var buffer = document.getElementById("canvas-buffer");
-  if (!buffer ) {
-    buffer = document.createElement("canvas");
-    buffer.id = "canvas-buffer";
-    buffer.style.display = "none";
-    document.body.appendChild(buffer);
-  } // if (!buffer)
-  buffer.width = renderWidth;
-  buffer.height = renderHeight;
+    buffer.width = renderWidth;
+    buffer.height = renderHeight;
 
-  // Determine scale factors
-  var hscale = imgWidth/renderWidth;
-  var vscale = imgHeight/renderHeight;
+    // Determine scale factors
+    var hscale = imgWidth/renderWidth;
+    var vscale = imgHeight/renderHeight;
 
-  // Set up how much we change x and y each time.
-  var deltaX = 2.0/renderWidth;
-  var deltaY = 2.0/renderHeight;
+    // Set up how much we change x and y each time.
+    var deltaX = 2.0/renderWidth;
+    var deltaY = 2.0/renderHeight;
 
-  // Get contexts for both canvases
-  var canvasContext = canvas.getContext("2d");
-  var bufferContext = buffer.getContext("2d");
+    // Get contexts for both canvases
+    var canvasContext = canvas.getContext("2d");
+    var bufferContext = buffer.getContext("2d");
 
-  // Set up the image data
-  var region = bufferContext.createImageData(renderWidth,renderHeight);
+    // Set up the image data
+    var region = bufferContext.createImageData(renderWidth,renderHeight);
 
-  // Set up the mouse (we don't want it changing while rendering).
-  var m = {
-    x: MIST.mouseX,
-    y: MIST.mouseY,
-    X: MIST.clickX,
-    Y: MIST.clickY
-  };
+    // Set up the mouse (we don't want it changing while rendering).
+    var m = {
+      x: MIST.mouseX,
+      y: MIST.mouseY,
+      X: MIST.clickX,
+      Y: MIST.clickY
+    };
 
-  // Build the function
-  var fun = MIST.expToRGB("untitled image", exp, context);
-  // Set up our main variables
-  var x = -1;
-  var y = -1 - deltaY;
+    // Build the function
+    var fun = MIST.expToRGB("untitled image", exp, context);
+    // Set up our main variables
+    var x = -1;
+    var y = -1 - deltaY;
 
-  // Loop through all of the pixels
-  for (var i = 0; i < region.data.length; i+= 4)
-    {
-      // When we reach the end of the row, move on to the next row
-      if ((i % (4*renderWidth)) == 0)
-        { 
-          x = -1;
-          y += deltaY;
-        } // if (i % (4*imgWidth)) == 0
+    // Loop through all of the pixels
+    for (var i = 0; i < region.data.length; i+= 4)
+      {
+        // When we reach the end of the row, move on to the next row
+        if ((i % (4*renderWidth)) == 0)
+          { 
+            x = -1;
+            y += deltaY;
+          } // if (i % (4*imgWidth)) == 0
 
-      // Evaluate the function
-      var rgb = fun(x,y,t,m);
+        // Evaluate the function
+        var rgb = fun(x,y,t,m);
 
-      // Exploration
-      // if (i < 4*imgWidth) { console.log("i",i, "x",x, "y",y, "rgb",rgb); }
- 
-      // Copy the pixels
-      region.data[i+0] = rgb[0];
-      region.data[i+1] = rgb[1];
-      region.data[i+2] = rgb[2];
-      region.data[i+3] = 255;
- 
-      // And advance to the next pixel
-      x += deltaX;
-    } // for
+        // Exploration
+        // if (i < 4*imgWidth) { console.log("i",i, "x",x, "y",y, "rgb",rgb); }
+   
+        // Copy the pixels
+        region.data[i+0] = rgb[0];
+        region.data[i+1] = rgb[1];
+        region.data[i+2] = rgb[2];
+        region.data[i+3] = 255;
+   
+        // And advance to the next pixel
+        x += deltaX;
+      } // for
 
-  // Random computations
-  var renderLeft = imgLeft*renderWidth/imgWidth;
-  var renderTop = imgTop*renderHeight/imgHeight;
+    // Random computations
+    var renderLeft = imgLeft*renderWidth/imgWidth;
+    var renderTop = imgTop*renderHeight/imgHeight;
 
-  // Draw and scale
-  bufferContext.putImageData(region, 0, 0);
-  canvasContext.drawImage(buffer, imgLeft, imgTop, imgWidth, imgHeight);
+    // Draw and scale
+    bufferContext.putImageData(region, 0, 0);
+    canvasContext.drawImage(buffer, imgLeft, imgTop, imgWidth, imgHeight);
 
-//canvasContext.fillStyle = "rgb(200, 0, 0)";
-//canvasContext.fillRect (10,10,75,50);
+  //canvasContext.fillStyle = "rgb(200, 0, 0)";
+  //canvasContext.fillRect (10,10,75,50);
 
-//return t;
-
-} // MIST.renderAt
-
+  //return t;
+  }
+})() // MIST.renderAt
